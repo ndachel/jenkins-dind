3265f9757d54823ec8315ff65941892aeceb46eb
Nels Dachel <ndachel@gmail.com>
2016-05-19 09:11:01 -0600

    named DPR

--- wrapper.sh
+++ wrapper.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "==> Launching the Docker daemon..."
-dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=overlay --insecure-registry=172.16.1.20:5000 &
+dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=overlay --insecure-registry=private-registry.marathon.mesos:5000 &
 
 while(! docker info > /dev/null 2>&1); do
     echo "==> Waiting for the Docker daemon to come online..."
